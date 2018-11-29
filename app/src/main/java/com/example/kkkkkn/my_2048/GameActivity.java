bdba309de2ffa3ea860b64ecd72b6f694a215f9b
kkkkkn <1185553557@qq.com>
2018-11-29 16:31:57 +0800

    项目初始化

---
+++ app/src/main/java/com/example/kkkkkn/my_2048/GameActivity.java
@@ -0,0 +1,17 @@
+package com.example.kkkkkn.my_2048;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        //初始化游戏界面，绘制游戏界面
+
+        //生成2个 2，位置随机
+    }
+}
